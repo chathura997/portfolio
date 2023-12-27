7f8c2cd75203790bd192002b64a8d9a930b0d167
chathura <chathuradinushka97@gmail.com>
2023-12-27 10:03:07 +0530

    add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import "./app.scss"
 import  Navbar  from './components/navbar/navbar'
+import Land from './components/land/Land'
+import Parallax from './components/parallax/Parallax'
 
 const App = () => {
   return (
     <div>
-      <section id='Homepage'> 
+      <section id='Homepage'>
         <Navbar/>
+        <Land/>
       </section>
       <section id='About'>About</section>
-      <section id='Projects'>Projects</section>
+      <section id='skils'><Parallax type="skils"/></section>
+      <section>Skils</section>
+      <section id="projects"><Parallax type="projects"/></section>
+      <section>Project1</section>
+      <section>Project2</section>
+      <section>Project3</section>
       <section id='Contact'>Contacts</section>
       
     </div>
-        // "Homepage",
-        // "About", 
-        // "Projects", 
-        // "Contacts", 
+     
     
   )
 }
